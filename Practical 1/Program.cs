4b6d4b17e203f84b2a03b79f233eaf911271d3ef
TornikeAm <82511782+TornikeAm@users.noreply.github.com>
2022-12-06 19:15:53 +0400

    Add files via upload
    First Practical Task.
    
    The program Returns the Maximum number of consecutive characters in the input string.

---
+++ Practical 1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Test
+{
+    class Program
+    {
+
+        static char maxRepeating(string str)
+{
+        int count = 0;
+        char res = str[0];
+        int cur_count = 1;
+ 
+       for (int i = 0; i < str.Length; i++)
+        {
+            if (i < str.Length - 1 &&
+                str[i] == str[i + 1])
+                cur_count++;
+ 
+            else
+            {
+                if (cur_count > count)
+                {
+                    count = cur_count;
+                    res = str[i];
+                }
+                cur_count = 1;
+            }
+        }
+    return res;
+}
+
+        static void Main(string[] args) {
+            while (true){
+                Console.WriteLine("Enter a String/ If Stop click n: ");
+                var input = Console.ReadLine();
+                if (input.ToLower() == "n"){
+                    break;   
+                }
+                Console.WriteLine("Most Repeated Consocutive char is : " + maxRepeating(input));
+
+            }
+
+            
+        }   
+    }
+
+}
