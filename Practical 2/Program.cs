f819ac1e258a5c7cbe15c0fbf4d47b79e25b0b04
TornikeAm <82511782+TornikeAm@users.noreply.github.com>
2022-12-20 12:08:54 +0400

    Add files via upload
    Practical 2 : Create a program that takes an integer in decimal, and a new base number system (from 2 to 20) from the command line. Print the original number converted to this system to the console.

---
+++ Practical 2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Test
+
+{
+
+    class Program
+    {
+        static void Main(String[] args){
+            Console.WriteLine("Input Value in Decimal: ");
+            int dec = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter The base : ");
+            int Base = int.Parse(Console.ReadLine());
+            Console.WriteLine("Converted {0} from Decimal To Chosen Base of {1} is {2}",dec,Base,FromDecimalToAnyBase(dec,Base));
+
+            }
+
+        static int FromDecimalToAnyBase(int Decimal, int Base ){
+            string two ="";
+            
+            while (Decimal > 0){
+                two = Decimal % Base + two;
+                Decimal = Decimal /Base;
+            }
+            return Convert.ToInt32(two);
+
+        }
+
+}}
