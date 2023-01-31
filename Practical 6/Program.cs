7205b40319fad18007b0dd64c4a6222253f34379
TornikeAm <82511782+TornikeAm@users.noreply.github.com>
2023-01-31 12:33:12 +0400

    Add files via upload

---
+++ Practical 6/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+
+    class program{
+        static void Main(string[] args){
+            Console.WriteLine("Testing BBC.Com/Sport with Css Locators and XPath.");
+
+        }
+    }
